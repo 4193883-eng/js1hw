8864d6f3e1ac75f0d223da649845dbdaccf31b28
Vsevolod <4193883@gmail.com>
2022-01-04 14:49:06 +0200

    JS #1

---
+++ script.js
@@ -0,0 +1,19 @@
+//Создать 2 переменные типа STRING
+let name = "Pablo"
+let year = "рік"
+//Создать 3 переменные типа NUMBER
+let age = 42069
+let one = 1
+let two = 2
+//Создать 2 переменные типа BOOLEAN
+let True = true
+let False = false
+//Вывести их все на экран.
+console.log("Це -", name+",", "йому", age, year)
+console.log(name)
+console.log(year)
+console.log(age)
+console.log(one)
+console.log(two)
+console.log(True)
+console.log(False)
